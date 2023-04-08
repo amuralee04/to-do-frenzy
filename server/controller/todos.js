ba12971a90c9814f5bddfd9090b7f7db2ea76e48
Aaran Muraleetharan <aaran.muraleetharan.2@gmail.comaaran.muraleetharan.2@gmail.comaaran.muraleetharan.2@gmail.com>
2023-04-08 15:13:15 -0400

    Commit framework updates

--- server/controller/todos.js
+++ server/controller/todos.js
@@ -5,9 +5,9 @@ export const readTodos = async(req,res) => {
 
     try {
         const todos =  await Todo.find();
-        res.Status(200).json(todos);
+        res.status(200).json(todos);
     } catch (error) {
-        res.Status(404).json({ error: error.message});
+        res.status(404).json({ error: error.message});
     }
 
 }
@@ -16,9 +16,9 @@ export const createTodos = async(req,res) => {
     const todo = new Todo(req.body);
     try {
         await todo.save();
-        res.Status(201).json(todo);
+        res.status(201).json(todo);
     } catch (error) {
-        res.Status(409).json({ error: error.message});
+        res.status(409).json({ error: error.message});
     }
 
 }
