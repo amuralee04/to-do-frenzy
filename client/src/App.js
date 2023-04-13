d81aa5987155991dc20e05e4b2b308ddfe592701
Aaran Muraleetharan <aaran.muraleetharan.2@gmail.comaaran.muraleetharan.2@gmail.comaaran.muraleetharan.2@gmail.com>
2023-04-13 14:51:46 -0400

    Initial Client Commit: Update and Delete Functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import { useEffect, useState} from "react";
+import { set } from "mongoose";
+import { useEffect, useState, props} from "react";
+import { deleteTodo, updateTodo } from "./api/index.js";
 import Preloader from "./components/Preloader.js";
 import { createTodo, readTodos } from "./functions/index.js";
 
@@ -8,6 +10,13 @@ function App() {
 
   const [todos, setTodos] = useState(null);
 
+  const [currentId, setCurrentId] = useState(0);
+
+  useEffect(() => {
+    let currentTodo = currentId !== 0 ? todos.find(todo => todo._id === currentId): {title:'', content: ''} 
+    setTodo(currentTodo)
+  }, [currentId, todos])
+
   useEffect(() => {
     const fetchData = async() => {
       const result = await readTodos();
@@ -15,13 +24,42 @@ function App() {
     }
 
     fetchData();
-  }, [])
+  }, [currentId])
+
+  const clear = () => {
+    setCurrentId(0);
+    setTodo({title:'', content: ''});
+  }
+
+  useEffect(() => {
+    const clearField = (e) => {
+      if(e.keyCode === 27) {
+        clear();
+      }
+    }
+    window.addEventListener('keydown', clearField)
+  return () => window.addEventListener('keydown', clearField)
+  })
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
 
-    const result = await createTodo(todo);
-    console.log(result);
+    if (currentId === 0) {
+      const result = await createTodo(todo);
+      setTodos([...todos, result]);
+      clear();
+    } else {
+      await updateTodo(currentId,todo);
+      clear();
+    }
+
+  }
+
+  const removeTodo = async(id) => {
+    await deleteTodo(id);
+    const todosCopy = [...todos];
+    todosCopy.filter(todo => todo._id !== id);
+    setTodos(todosCopy);
   }
 
   return (
@@ -33,14 +71,16 @@ function App() {
           <div className="row">
             <div className="input-field col s6">
               <i className="material-icons prefix">title</i>
-              <input id="icon_prefix" type="text" className="validate" 
+              <input id="icon_prefix" type="text" className="validate"
+              value={todo.title} 
               onChange={e => setTodo({...todo, title: e.target.value})}
               />
               <label htmlFor="icon_prefix">Title</label>
             </div>
             <div className="input-field col s6">
               <i className="material-icons prefix">description</i>
-              <input id="description" type="tel" className="validate" 
+              <input id="description" type="tel" className="validate"
+              value={todo.content} 
               onChange={e => setTodo({...todo, content: e.target.value})}
               />
               <label htmlFor="description">Content</label>
@@ -54,8 +94,12 @@ function App() {
           !todos? <Preloader/>: todos.length>0?
           <ul className="collection">
           {todos.map(todo => (
-            <li key={todo._id} className="collection-item"><div><h5>{todo.title}</h5>
-            <p>{todo.content}<a href="#!" className="secondary-content">
+            <li key={todo._id} 
+            onClick={() => setCurrentId(todo._id)}
+            className="collection-item"><div><h5>{todo.title}</h5>
+            <p>{todo.content}<a href="#!" 
+            onClick={() => removeTodo(todo._id)}
+            className="secondary-content">
             <i className="material-icons">delete</i></a></p></div></li>
           ))}
         </ul> : <div><h5>Nothing to do</h5></div>
--- server/controller/todos.js
+++ server/controller/todos.js
@@ -22,3 +22,29 @@ export const createTodo = async(req,res) => {
     }
 
 }
+
+export const updateTodo = async(req,res) => {
+    const {id} = req.params;
+    const {title, content} = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send(`The id ${id} is not valid.`);
+    }
+
+    const todo = {title, content, _id:id};
+    await Todo.findByIdAndUpdate(id, todo,{new:true})
+    res.json(todo);
+
+}
+
+export const deleteTodo = async(req,res) => {
+    const {id} = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send(`The id ${id} is not valid.`);
+    }
+
+    await Todo.findByIdAndRemove(id);
+    res.json({message:'Todo deleted successfully.'});
+
+}
